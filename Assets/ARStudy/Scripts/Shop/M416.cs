650cbb16115e1ac27ee9f81e63b3eb4ecb568b69
dbrud951 <dbrud951@hanmail.net>
2018-10-25 15:18:03 +0900

    M416.cs에 무기 정보 저장

--- Assets/ARStudy/Scripts/Shop/M416.cs
+++ Assets/ARStudy/Scripts/Shop/M416.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class M416 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public string explain = "이것은 M416이다.";
+	public int price = 200; 
+	public string name = "M416";
+	public int damage = 200;
+	public Sprite image;
+
+
+	public int Price{
+		get{
+			return price;
+		}
 	}
+	public int Damage{
+		get{
+			return damage;
+		}
+	}
+	public string Explain{
+		get{
+			return explain;
+		}
+	}
+
+	public string Name{
+		get{
+			return name;
+		}
+	}
+
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
