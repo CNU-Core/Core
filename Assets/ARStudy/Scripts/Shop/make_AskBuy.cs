f92200d85b8e4fe7c2f34b543f943f8cf511a898
dbrud951 <dbrud951@hanmail.net>
2018-10-25 16:10:13 +0900

    make_AskBuy.cs에 make_AskSelect_window 함수 작성 : 구매 의사를 뭍는 팝업창을 구성하는 기능

--- Assets/ARStudy/Scripts/Shop/make_AskBuy.cs
+++ Assets/ARStudy/Scripts/Shop/make_AskBuy.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class make_AskBuy : MonoBehaviour {
+	//public Image selectedGun_Image;
+	public Image show_Image;
+	public Text show_price;
+	public Text show_name;
+	public void make_AskSelect_window(){
+		show_name.GetComponent<Text>().text = ShopManager.instance.select_name.GetComponent<Text>().text;
+		show_price.GetComponent<Text>().text = ShopManager.instance.select_price.GetComponent<Text>().text;
+		show_Image.GetComponent<Image>().sprite = ShopManager.instance.select_image.GetComponent<Image>().sprite;
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
