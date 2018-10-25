7edabd12073921b68d5e95f381fc8743eefa9550
dbrud951 <dbrud951@hanmail.net>
2018-10-25 15:51:00 +0900

    PotionRed.cs에 setData 함수 작성 : Red의 정보를 ShopManager에게 전달하는 기능

--- Assets/ARStudy/Scripts/Shop/PotionRed.cs
+++ Assets/ARStudy/Scripts/Shop/PotionRed.cs
@@ -10,6 +10,14 @@ public class PotionRed : MonoBehaviour {
 	public int heal = 100;
 	public Sprite image;
 
+		public void setData(){
+		ShopManager.Instance.potion_price = this.price;
+		ShopManager.Instance.potion_heal = this.heal;
+		ShopManager.Instance.potion_explain = this.explain;
+		ShopManager.Instance.potion_name = this.name;
+		ShopManager.Instance.potion_image = this.image;
+		ShopManager.Instance.setting();
+	}
 
 	public int Price{
 		get{
