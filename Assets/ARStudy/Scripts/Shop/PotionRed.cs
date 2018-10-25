58daa8fd069ccd35a58c0ac06fe631a49e11cc0d
dbrud951 <dbrud951@hanmail.net>
2018-10-25 15:49:08 +0900

    PotionRed.cs 생성 :  포션 Red의 정보를 담고 있다.

--- Assets/ARStudy/Scripts/Shop/PotionRed.cs
+++ Assets/ARStudy/Scripts/Shop/PotionRed.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PotionRed : MonoBehaviour {
+	public string explain = "이것은 빨간 포션이다.";
+	public int price = 100; 
+	public string name = "Red";
+	public int heal = 100;
+	public Sprite image;
 
-	// Use this for initialization
-	void Start () {
-		
+	
+
+	public int Price{
+		get{
+			return price;
+		}
+	}
+	public int Heal{
+		get{
+			return heal;
+		}
+	}
+	public string Explain{
+		get{
+			return explain;
+		}
+	}
+
+	public string Name{
+		get{
+			return name;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
