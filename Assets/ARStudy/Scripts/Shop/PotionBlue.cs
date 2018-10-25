11a9118d414a9627bc43d850fae85a29db08b145
dbrud951 <dbrud951@hanmail.net>
2018-10-25 15:54:34 +0900

    PotionBlue.cs에 포션 정보 저장

--- Assets/ARStudy/Scripts/Shop/PotionBlue.cs
+++ Assets/ARStudy/Scripts/Shop/PotionBlue.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PotionBlue : MonoBehaviour {
+public string explain = "이것은 파란 포션이다.";
+	public int price = 200; 
+	public string name = "Blue";
+	public int heal = 200;
+	public Sprite image;
 
-	// Use this for initialization
-	void Start () {
-		
+
+	public int Price{
+		get{
+			return price;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public int Heal{
+		get{
+			return heal;
+		}
+	}
+	public string Explain{
+		get{
+			return explain;
+		}
+	}
+
+	public string Name{
+		get{
+			return name;
+		}
 	}
 }
