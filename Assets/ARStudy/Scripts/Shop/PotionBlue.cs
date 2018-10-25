5d20aaf8d903537735538c8b970395689f3c65dd
dbrud951 <dbrud951@hanmail.net>
2018-10-25 15:55:28 +0900

    PotionRED.cs에 setData함수 작성 : Blu의 정보를 ShopManager에게 전달하는 기능

--- Assets/ARStudy/Scripts/Shop/PotionBlue.cs
+++ Assets/ARStudy/Scripts/Shop/PotionBlue.cs
@@ -10,6 +10,14 @@ public string explain = "이것은 파란 포션이다.";
 	public int heal = 200;
 	public Sprite image;
 
+		public void setData(){
+		ShopManager.Instance.potion_price = this.price;
+		ShopManager.Instance.potion_heal = this.heal;
+		ShopManager.Instance.potion_explain = this.explain;
+		ShopManager.Instance.potion_name = this.name;
+		ShopManager.Instance.potion_image = this.image;
+		ShopManager.Instance.setting();
+	}
 
 	public int Price{
 		get{
