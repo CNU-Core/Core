efed4c5e4ff0cbfe99fc7240c34ef5ec81b7f88b
LeeHeungrok <rhh0208@naver.com>
2018-11-09 02:20:36 +0900

    GamesManager Don't Destroyed 화 시킴 ARSurvive 씬 기본화면 수정

--- Assets/ARSurvive/Scripts/GamesManager.cs
+++ Assets/ARSurvive/Scripts/GamesManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GamesManager : MonoBehaviour {
+	private static GamesManager Instance;
 
 	// Use this for initialization
 	void Start () {
-		
+		if(Instance != null){
+			GameObject.Destroy(gameObject);
+		}
+		else {
+			GameObject.DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}	
 	}
 	
 	// Update is called once per frame
